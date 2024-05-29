690aa429865bf849f61d983468671534f1e7043c
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-05-29 22:00:07 +0200

    unfinishedtask3

---
+++ js/task3.js
@@ -0,0 +1,17 @@
+class StringBuilder {
+  #value;
+  constructor(value) {
+    this.#value = value;
+  }
+  getValue() {
+    return this.#value;
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
