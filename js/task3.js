4dd67b4b07d296135d44db3a4da0468b509f69d0
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-05-31 09:55:38 +0200

    task123done

--- js/task3.js
+++ js/task3.js
@@ -6,6 +6,15 @@ class StringBuilder {
   getValue() {
     return this.#value;
   }
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
